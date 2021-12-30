313bd0d6a4f5f895040a0139868d6307edff9569
Quang Bao <hoangquangbao.developer@gmail.com>
2021-12-30 09:17:48 +0700

    Initial Commit

---
+++ AnimatedCarouselSlide/AnimatedCarouselSlideApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedCarouselSlideApp.swift
+//  AnimatedCarouselSlide
+//
+//  Created by Quang Bao on 30/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedCarouselSlideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedCarouselSlide/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimatedCarouselSlide
+//
+//  Created by Quang Bao on 30/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
